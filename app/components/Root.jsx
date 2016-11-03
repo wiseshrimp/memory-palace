b85d4e70c2fd17b74b5810fb23197a123982d37e
mithunder <mselvaratnam@gmail.com>
2016-11-03 12:54:15 -0400

    static product page works

--- app/components/Root.jsx
+++ app/components/Root.jsx
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import Home from './Home';
+import Product from './Product';
 
 export default class Root extends Component {
     render() {
         return (
             <div>
                 <Navbar />
-                <Home />
+                <Product />
                 <Footer />
-            </div>    
+            </div>
         )
     }
 }
