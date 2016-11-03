c751528feb1599b7abe0c7da1bd817d79d16e037
Sue Roh <sr3022@columbia.edu>
2016-11-03 11:03:10 -0400

    Added containers folder

---
+++ app/components/Cart.jsx
@@ -0,0 +1,13 @@
+'use strict'
+
+import React from 'react';
+
+export default class Cart extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        
+    }
+}
