4da6ba943960f2a02963be02b413f90b972b3c3e
Sue Roh <sr3022@columbia.edu>
2016-11-04 21:53:50 -0400

    Fixed routes to use react router

--- app/components/SearchBar.jsx
+++ app/components/SearchBar.jsx
@@ -1,15 +1,14 @@
 'use strict'
 
 import React from 'react';
-import Router from 'react-router';
+import { Link } from 'react-router';
 
 export default class SearchBar extends React.Component {
-    constructor(props, context) {
-        super(props, context);
+    constructor(props) {
+        super(props);
         this.state = {
             currentTerm: ''
         }
-        this.context = context;
         this.onInputChange = this.onInputChange.bind(this);
         this.onSearchSubmit = this.onSearchSubmit.bind(this);
     }
@@ -22,9 +21,6 @@ export default class SearchBar extends React.Component {
 
     onSearchSubmit(e) {
         e.preventDefault();
-        this.context.router.push({
-            pathname: `search/${this.state.currentTerm}`
-        })
     }
 
     render() {
@@ -37,10 +33,11 @@ export default class SearchBar extends React.Component {
                         <div className="input-group">
                         <input type="text" className="form-control" placeholder="Search" name="srch-term" id="srch-term" onChange={this.onInputChange} />
                             <div className="input-group-btn">
-                            <button className="btn btn-default" type="submit">
-                                <i className="glyphicon glyphicon-search">
-                                </i>
+                            <Link to={`search/${this.state.currentTerm}`}>
+                                <button className="btn btn-default" type="submit">
+                                    <i className="glyphicon glyphicon-search" />
                             </button>
+                            </Link>
                             </div>
                         </div>
                     </form>
--- app/components/SearchedItemList.jsx
+++ app/components/SearchedItemList.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 
 export default class SearchedItemList extends React.Component {
     render() {
-        console.log(this.props);
-        if (this.props.products) {
+        let {products, routeParams} = this.props;
+        if (products) {
             return (
                 <div className="row">
-                    {this.props.products.map(product => (
+                    <h4>{products.length} results for {routeParams.term}</h4>
+                    {products.map(product => (
                         <div key={product.id} className="col-sm-6 col-md-3">
                             <div className="thumbnail">
-                                <img src={ product.imageUrl} />
+                                <img src={product.imageUrl} />
                                 <div className="caption">
                                     <h3>{product.title}</h3>
                                     <p><a href="#" className="btn btn-primary" role="button">Add To Cart</a></p>
