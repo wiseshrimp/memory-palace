de001ef13d20fee40dce8ecdee527161ebbae947
Sue Roh <sr3022@columbia.edu>
2016-11-05 14:59:34 -0400

    Added passwords match feature check

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -26,6 +26,14 @@ export default class Login extends React.Component {
         });
     }
 
+    checkPasswordMatch() {
+        var password = $("#password").val();
+        var confirmPassword = $("#confirm_password").val();
+
+        if (password != confirmPassword)
+            $("#password_message").html("Passwords do not match!");
+        }
+
     render() {
         return (
             <div className="container">
@@ -48,15 +56,15 @@ export default class Login extends React.Component {
                                 <div className="col-lg-12">
                                     <form id="login-form" action="#" method="post" role="form">
                                         <div className="form-group">
-                                            <input type="text" name="username" id="username" tabindex="1" className="form-control" placeholder="Username" />
+                                            <input type="text" name="username" id="username" tabIndex="1" className="form-control" placeholder="Email Address" />
                                         </div>
                                         <div className="form-group">
-                                            <input type="password" name="password" id="password" tabindex="2" className="form-control" placeholder="Password" />
+                                            <input type="password" name="password" id="password" tabIndex="2" className="form-control" placeholder="Password" />
                                         </div>
                                         <div className="form-group">
                                             <div className="row">
                                                 <div className="col-sm-6 col-sm-offset-3">
-                                                    <input type="submit" name="login-submit" id="login-submit" tabindex="4" className="form-control btn btn-login" value="Log In" />
+                                                    <input type="submit" name="login-submit" id="login-submit" tabIndex="4" className="form-control btn btn-login" value="Log In" />
                                                 </div>
                                             </div>
                                         </div>
@@ -64,7 +72,7 @@ export default class Login extends React.Component {
                                             <div className="row">
                                                 <div className="col-lg-12">
                                                     <div className="text-center">
-                                                        <a href="#" tabindex="5" class="forgot-password">Forgot Password?</a>
+                                                        <a href="#" tabIndex="5" className="forgot-password">Forgot Password?</a>
                                                     </div>
                                                 </div>
                                             </div>
@@ -72,21 +80,19 @@ export default class Login extends React.Component {
                                     </form>
                                     <form id="register-form" action="#" method="post" role="form">
                                         <div className="form-group">
-                                            <input type="text" name="username" id="username" tabindex="1" className="form-control" placeholder="Username" />
+                                            <input type="email" name="email" id="email" tabIndex="1" className="form-control" placeholder="Email Address" />
                                         </div>
                                         <div className="form-group">
-                                            <input type="email" name="email" id="email" tabindex="1" className="form-control" placeholder="Email Address" />
+                                            <input type="password" name="password" id="password" tabIndex="2" className="form-control" placeholder="Password" />
                                         </div>
                                         <div className="form-group">
-                                            <input type="password" name="password" id="password" tabindex="2" className="form-control" placeholder="Password" />
-                                        </div>
-                                        <div className="form-group">
-                                            <input type="password" name="confirm-password" id="confirm-password" tabindex="2" className="form-control" placeholder="Confirm Password" />
+                                            <h5 id="password_message"></h5>
+                                            <input type="password" name="confirm_password" id="confirm_password" tabIndex="2" className="form-control" onChange={this.checkPasswordMatch} placeholder="Confirm Password" />
                                         </div>
                                         <div className="form-group">
                                             <div className="row">
                                                 <div className="col-sm-6 col-sm-offset-3">
-                                                    <input type="submit" name="register-submit" id="register-submit" tabindex="4" className="form-control btn btn-register" value="Register Now" />
+                                                    <input type="submit" name="register-submit" id="register-submit" tabIndex="4" className="form-control btn btn-register" value="Register Now" />
                                                 </div>
                                             </div>
                                         </div>
