60416c11acaff6480495917b76e57c8e39f8819f
Stephanie Manwaring <stephanie.manwaring@gmail.com>
2016-11-08 20:12:22 -0500

    added jumbotron

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -4,10 +4,19 @@ import ProductList from '../containers/ProductList';
 export default class Home extends React.Component {
     render() {
       return (
-          <div>
-              <h3 className="main-header">MOST POPULAR</h3>
-              <ProductList />
-          </div>
+      	<div>
+	        <div className="jumbotron">
+		      <div className="container">
+		        <h1>Memories like you've never remembered...</h1>
+		        <p></p>
+		        <p><a class="btn btn-primary btn-lg" href="#" role="button">BROWSE MEMORIES »</a></p>
+		      </div>
+		    </div>
+	        <div className="add-padding">
+	              <h3 className="main-header">MOST POPULAR</h3>
+	              <ProductList />
+	        </div>
+	    </div>
       )
   }
 }
